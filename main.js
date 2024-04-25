cf20abd7c56a736977b373eb2aaa1b5fa3d607b1
oneazi <omairneazi2000@gmail.com>
2024-04-24 19:02:28 -0700

    Removed comments

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 baseURL = 'http://localhost:5000/'
 
-// function buttonPress() {
-//     console.log("pressed button")
-// }
+function sendImage() {
 
-// document.getElementById('fileupload').addEventListener('change', previewImage, false);
-
-// function previewImage(evt) { 
-// 	var files = evt.target.files; 
-// 	var f = files[0]; 
-// 	var reader = new FileReader();
-// 	reader.onload = (
-// 		function(theFile) { 
-// 			return function(e) { 
-// 				document.getElementById('imagePreview').innerHTML = [
-// 					'<img src="', e.target.result,'" title="', theFile.name, '" width=250" />'
-// 					].join('');
-// 			}; 
-// 		}
-// 	)(f);
-// 	reader.readAsDataURL(f); 
-// }
-
-// function sendImage() {
-
-// }
+}
 
 function getBarcode(elementID) {
 	console.log(`Clicked the ${elementID} button`)
